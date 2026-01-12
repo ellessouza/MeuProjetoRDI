166c5313a452981bb307befbeba9c804708d1b8c
Adryelle S Santos <adryellessouza@gmail.com>
2026-01-12 20:51:00 -0300

    Atualizando Program.cs com o código da tabuada

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.Write("Digite qual tabuada você deseja ver (1 a 10): ");
+        
+        if (int.TryParse(Console.ReadLine(), out int numero))
+        {
+            if (numero >= 1 && numero <= 10)
+            {
+                Console.WriteLine($"\n--- Tabuada do {numero} ---");
+
+                for (int j = 10; j >= 1; j--)
+                {
+                    Console.WriteLine($"{numero} x {j} = {numero * j}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Erro: Por favor, insira um valor entre 1 e 10.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Erro: Entrada inválida. Digite apenas números.");
+        }
+
+        Console.WriteLine("\nPressione qualquer tecla para sair...");
+        Console.ReadKey();
